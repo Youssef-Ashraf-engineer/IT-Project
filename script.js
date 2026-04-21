468473237afcd576ffc83259e400cbc9f7177149
Your Name <you@example.com>
2026-04-21 03:17:37 +0200

    changes in search ui

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function search(){
     let hotels = document.querySelectorAll(".hotel-card")
-    let placevalue = document.querySelector("#palce-select").value
-    let datevalue = document.querySelector("#Date-select").value
+    let placevalue = document.querySelector("#place").value
+    let datevalue = document.querySelector("#date").value
     let numbervalue = document.querySelector("#number-input").value
     hotels.forEach(element => {
         let place = element.dataset.place;
@@ -21,3 +21,38 @@ function search(){
         }
     });
 }
+
+const input = document.getElementById("searchInput");
+const dropdown = document.getElementById("dropdown");
+const place = document.getElementById("place");
+
+
+input.addEventListener("focus", () => {
+  dropdown.style.display = "block";
+});
+
+document.querySelectorAll(".dropdown div").forEach(item => {
+  item.onclick = () => {
+    input.value = item.textContent;
+    dropdown.style.display = "none";
+    place.value=item.textContent;
+  };
+});
+
+
+const dateinput = document.getElementById("dateInput");
+const datedropdown = document.getElementById("datedropdown");
+const date = document.getElementById("date");
+
+
+dateinput.addEventListener("focus", () => {
+  datedropdown.style.display = "block";
+});
+
+document.querySelectorAll("#datedropdown div").forEach(item => {
+  item.onclick = () => {
+    dateinput.value = item.textContent;
+    datedropdown.style.display = "none";
+    date.value=item.textContent;
+  };
+});
