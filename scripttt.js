0b79f11ae7f44dbe67508d1bd3908516cb4cf571
uoeel <uoeelrostom@gmail.com>
2026-05-03 18:04:21 +0300

    change of regitser page

--- scripttt.js
+++ scripttt.js
@@ -1,71 +1,3 @@
-const myRegister = document.getElementById("register");
-
-myRegister.addEventListener('submit', function (event) {
-    // will appear error if user do not give the first name 
-
-    const firstName = document.getElementById("firstName").value;
-    const error1 = document.getElementById("firstNameError");
-    error1.textContent = "";
-    if (firstName === "") {
-        event.preventDefault();
-        error1.textContent = "Please Enter your First Name";
-    }
-
-    // will appear error if user do not give the last name
-
-    const lastName = document.getElementById("lastname").value;
-    const error2 = document.getElementById("lastNameError");
-    error2.textContent = "";
-    if (lastName === "") {
-        event.preventDefault();
-        error2.textContent = "Please Enter Your Last Name";
-    }
-
-    // will appear error if user do not give the email
-
-    const email = document.getElementById("email").value;
-    const error3 = document.getElementById("emailll");
-    error3.textContent = "";
-    if (email === "") {
-        event.preventDefault();
-        error3.textContent = "Please Enter Your Email";
-    }
-
-    // will appear error if user do not give the password
-
-    const password = document.getElementById("pass").value;
-    const error4 = document.getElementById("PSSWORD");
-    error4.textContent = "";
-    if (password === "") {
-        event.preventDefault();
-        error4.textContent = "Please Enter Your Password";
-    }
-
-    // will appear error if user do not give the password
-
-    const password2 = document.getElementById("pass2").value;
-    const error5 = document.getElementById("PSSWORD2");
-    error5.textContent = "";
-    if (password2 === "" || password !== password2) {
-        event.preventDefault();
-        if (password2 === "") { error5.textContent = "Please Confirm Your Password"; }
-        if (password !== password2) { error5.textContent = "Password Do not Match" }
-    }
-
-
-    // will appear error if user do not give the ID national
-
-    const ID = document.getElementById("idnumber").value;
-    const error6 = document.getElementById("IDNumber");
-    error6.textContent = "";
-    if (ID === "" || ID !== 14) {
-        event.preventDefault();
-        if (ID === "") { error6.textContent = "Please Enter Your ID National"; }
-        if (ID !== 14 && ID > 0) { error6.textContent = "ID must be 14 Numbers"; }
-    }
-
-})
-
 // Local storage
 
 let UserName = document.querySelector('#firstName');
@@ -82,8 +14,84 @@ let submit = document.querySelector('#submit');
 let arrayOfTask = localStorage.getItem("allUsers") ?
 JSON.parse(localStorage.getItem("allUsers")):[];
 
+
+const myRegister = document.getElementById("register");
+
+
+myRegister.addEventListener('submit', function (event) {
+    event.preventDefault();
+    // will appear error if user do not give the first name 
+    let existerror = false ;
+
+    const firstName = document.getElementById("firstName").value;
+    const error1 = document.getElementById("firstNameError");
+    error1.textContent = "";
+    if (firstName === "") {
+        event.preventDefault();
+        error1.textContent = "Please Enter your First Name";
+        existerror = true ;
+    }
+
+    // will appear error if user do not give the last name
+
+    const lastName = document.getElementById("lastname").value;
+    const error2 = document.getElementById("lastNameError");
+    error2.textContent = "";
+    if (lastName === "") {
+        event.preventDefault();
+        error2.textContent = "Please Enter Your Last Name";
+        existerror = true ;
+    }
+
+    // will appear error if user do not give the email
+
+    const email = document.getElementById("email").value;
+    const error3 = document.getElementById("emailll");
+    error3.textContent = "";
+    if (email === "") {
+        event.preventDefault();
+        error3.textContent = "Please Enter Your Email";
+        existerror = true ;
+    }
+
+    // will appear error if user do not give the password
+
+    const password = document.getElementById("pass").value;
+    const error4 = document.getElementById("PSSWORD");
+    error4.textContent = "";
+    if (password === "") {
+        event.preventDefault();
+        error4.textContent = "Please Enter Your Password";
+        existerror = true ;
+    }
+
+    // will appear error if user do not give the password
+
+    const password2 = document.getElementById("pass2").value;
+    const error5 = document.getElementById("PSSWORD2");
+    error5.textContent = "";
+    if (password2 === "" || password !== password2) {
+        event.preventDefault();
+        if (password2 === "") { error5.textContent = "Please Confirm Your Password"; }
+        if (password !== password2) { error5.textContent = "Password Do not Match" }
+        existerror = true ;
+    }
+
+
+    // will appear error if user do not give the ID national
+
+    const ID = document.getElementById("idnumber").value;
+    const error6 = document.getElementById("IDNumber");
+    error6.textContent = "";
+    if (ID === "" || ID.length !== 14) {
+        event.preventDefault();
+        if (ID === "") { error6.textContent = "Please Enter Your ID National"; }
+        if (ID.length !== 14 && ID.length > 0) { error6.textContent = "ID must be 14 Numbers"; }
+        existerror = true ;
+    }
+
 // submit task
-submit.onclick = function (e) {
+if (!existerror){
     
     addTaskToArray(
     UserName.value ,
@@ -103,7 +111,9 @@ submit.onclick = function (e) {
     Number1.value = "";
     Birth.value = "";
     Photo.value = "";
-}
+};
+
+});
 
 function addTaskToArray(username, lastname, email, password, idnumber, numbers, birth, photo) {
     const task = {
@@ -119,3 +129,4 @@ function addTaskToArray(username, lastname, email, password, idnumber, numbers, 
     arrayOfTask.push(task);
     window.localStorage.setItem("allUsers",JSON.stringify(arrayOfTask));
 }
+
